f70b265996478a5273e41a839e1cee178a085d3b
weberton Kaic <webertonk23@gmail.com>
2024-03-01 13:42:48 -0300

    limpando trechos comentados

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     // return view('welcome');
-// });
-
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
